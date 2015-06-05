3515f24f0e3e824cc74980e32ee53830dc07beff
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-06-05 17:03:07 -0400

    added and changed a few things, hopefully it works on the server

--- http/assets/js/settings.js
+++ http/assets/js/settings.js
@@ -1,7 +1,6 @@
-var SETTINGS = {
-	ip: "192.168.0.105"
-}
-
 // var SETTINGS = {
-// 	ip: "107.161.159.47:80" // server	
+// 	ip: "192.168.0.105:8000"
 // }
+var SETTINGS = {
+	ip: "107.161.159.47:80" // server	
+}
