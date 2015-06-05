69eebea213070a26408e96fcbf45f04203d5056c
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-06-05 12:49:34 -0400

    change a few things for the server

--- http.js
+++ http.js
@@ -10,7 +10,7 @@ var http = {
 		http.server = http.http.createServer(http.app);
 	},
 	start: function() {
-		http.server.listen(8000, "192.168.0.105");
+		http.server.listen(80, "107.161.159.47");
 		console.log("http server starting on port 8000")
 	}
 }
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 PATH=${PATH}:/home/bohdan/.nvm/v0.10.36/bin
 
 forever list
-forever start /srv/www/demo.bohdananderson.com/app/app.js
+forever start /srv/www/demo.bohdananderson.com/main.js
