6216b539a2e70adc384668c141c4c5f4398cc115
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-09-17 23:50:17 -0400

    cleaned up the code :D)

--- http/demos/game/game.js
+++ http/demos/game/game.js
@@ -1,73 +1,66 @@
-app.game = {
+var GAME = {
 	fullscreen: false,
 	init: function() {
 
-		if (Cookies.get('type')) {
-			app.game.phase3.init();
+		if (Cookies.get('element')) {
+			GAME.phase3.init();
 		} else {
-			app.game.phase1.init();
+			GAME.phase1.init();
 		}
 
 	}
 }
 
-app.game.phase1 = {
+// PREAMBLE
+GAME.phase1 = {
 	init: function() {
 		$(document.body).removeClass().addClass("phase1");
-		$("#user_to_phase2").click(app.game.phase1.next_phase);
+		$("#user_to_phase2").click(GAME.phase1.next_phase);
 	},
 	next_phase: function(event) {
 		event.preventDefault();
-		if (screenfull.enabled && app.game.fullscreen == true) {
+		if (screenfull.enabled && GAME.fullscreen == true) {
 			screenfull.request();
 		}
-		$(document.body).removeClass("phase1").addClass("phase2");
-		app.game.phase2.init();
+		GAME.phase2.init();
 		return false;
 	}
 }
 
-
-app.game.phase2 = {
+// Element Type
+GAME.phase2 = {
 	init: function() {
 		$(document.body).removeClass().addClass("phase2");
-		$("#user_choose_type a").click(app.game.phase2.set_type);
+		$("#user_choose_type a").click(GAME.phase2.set_type);
 	},
 	set_type: function(event) {
 		event.preventDefault();
-		console.log(this.href.split("#").pop())
-		Cookies.set('type', this.href.split("#").pop());
-		app.game.phase2.next_phase(event);
+		Cookies.set('element', this.href.split("#").pop());
+		GAME.phase2.next_phase(event);
 		return false;
 	},
 	next_phase: function(event) {
 		event.preventDefault();
-		$(document.body).removeClass("phase2").addClass("phase3");
-		app.game.phase3.init();
+		GAME.phase3.init();
 		return false;
 	}
 }
 
-app.game.phase3 = {
+// The first step in pairing!
+GAME.phase3 = {
 	init: function(error) {
-		app.data.element = Cookies.get('type');
-		if (!app.data.element) {
-			app.game.phase2.init();
+		PAIR.D.element = Cookies.get('element');
+		PAIR.init();
+		if (!PAIR.D.element) {
+			GAME.phase2.init();
 			return false;
 		}
 		if (!error) {
 			error = "";
 		}
+		APP.pr(error);
 		$(document.body).removeClass().addClass("phase3 " + error);
-		if (!app.socket) {
-			app.socket = io.connect(SETTINGS.ip);
-			app.socket.on("wait 2", app.wait);
-			app.socket.on("time paired 3", app.found_potential_pair);
-			app.socket.on("quarter check 5", app.quart_check);
-			app.socket.on("final check 7", app.final_check);
-			app.socket.on("stop", app.just_stop);
-			$("#pairing_button").click(app.clickButton);
-		}
+		//Start the pairing process
 	}
 }
 
@@ -75,22 +68,22 @@ app.game.phase3 = {
 
 
 
-app.game.phase4 = {
+GAME.phase4 = {
 	init: function() {
 		$(document.body).removeClass().addClass("phase4 ");
 	}
 }
 
-app.game.phase5 = {
+GAME.phase5 = {
 	init: function(elements) {
 		$(document.body).removeClass().addClass("phase5 ");
 		console.log(elements);
-		$("#pairing_success").html(elements[0] + " " + elements[1]);
-		$("#paired_keep_playing").click(app.game.phase5.keep_playing);
+		$("#pairing_success").html(elements);
+		$("#paired_keep_playing").click(GAME.phase5.keep_playing);
 	},
 	keep_playing: function(event) {
 		event.preventDefault();
-		app.game.phase3.init();
+		GAME.phase3.init();
 		return false;
 	}
 }
--- server/game.js
+++ server/game.js
@@ -29,118 +29,131 @@ var fakeSocket = function() {
 var possible_pairs_root = function(main_socket) {
 	var out = {
 		this_socket: main_socket,
-		pairs: [fakeSocket(), fakeSocket()], //it should be that     fakeList, //
+		pairs: [], //it should be that     fakeList, //
 		paired: null,
-		message_pairs: function(message_name, data) {
-			console.log("device: \t" + out.this_socket.id);
-			if (!out.pairs.length) {
-				console.log(out.this_socket.id + " found no pairs");
-				return false;
-			}
-			out.this_socket.emit(message_name, data)
-			for (var a = 0, max = out.pairs.length; a < max; ++a) {
-				console.log(" --\t\t" + out.pairs[a].id);
-				out.pairs[a].emit(message_name, data)
-			}
-		},
-		message_winners: function(message_name, data) {
-			console.log("winners: \t" + out.this_socket.id);
-			console.log("\t\t" + out.paired.id);
-			out.this_socket.emit(message_name, data);
-			out.paired.emit(message_name, data);
-		},
-		message_losers: function(message_name, data) {
-			for (var a = 0, max = out.pairs.length; a < max; ++a) {
-				if (out.pairs[a].id != out.paired.id) {
-					out.pairs[a].emit(message_name, data)
-				}
-			}
-		},
-		quarter_check: function() {
-			console.log("\nquarter check")
-			out.message_pairs("quarter check 5", out.pairs.length);
-		},
-
 		check: function() {
-			console.log("\n\n\nfinal check " + out.this_socket);
-
-			out.paired = out.find_closests_std(out.this_socket, out.pairs, 0.26, 0.1);
-
-			if (!out.paired) {
-				// out.this_socket.emit("final check 7", {
-				// 	"status": "failed"
-				// });
-
-				out.message_pairs("final check 7", {
-					"status": "failed"
-				});
-				// out.this_socket.pairing_data = null;
+			if (!out.pairs.length) {
 				return false;
 			}
-
-			out.paired.pairing_data.taken = true;
-
-			console.log(out.this_socket.pairing_data.std)
-			console.log(out.paired.pairing_data.std)
-			if (out.pairs.length >= 1) {
-				out.message_winners("final check 7", {
-					"status": "success",
-					"pairs": [out.this_socket.pairing_data.element, out.paired.pairing_data.element]
-				});
-				out.message_losers("final check 7", {
-					"status": "failed"
-				})
-				// out.this_socket.pairing_data = null;
-			} else {
-				console.log("\n\n\n SOMETHING WENT VERY WRONG \n\n\n");
-				out.message_pairs("final check 7", false);
+			var msg = out.this_socket.id + "<br>";
+			for (var a = 0, max = out.pairs.length; a < max; ++a) {
+				msg += out.pairs[a].id + "<br>";
 			}
 
-		},
-		find_closests_std: function(ref, list, limit, agr) {
-			var out = false,
-				arg_out = false,
-				min = limit || 99999999,
-				loc = 0,
-				agr_min = agr,
-				agr_loc = 0;
-
-			console.log("--\t" + ref.id)
-			// console.log(ref.pairing_data.table)
-			for (var a = 0, max = list.length; a < max; ++a) {
-
-				// console.log(list[a].pairing_data.table)
-				if (list[a].pairing_data && !list[a].pairing_data.taken && list[a].id != ref.id) {
-					agr_loc = 0;
-					agr_loc += Math.abs(ref.pairing_data.agr[0] - list[a].pairing_data.agr[0]);
-					agr_loc += Math.abs(ref.pairing_data.agr[1] - list[a].pairing_data.agr[1]);
-					agr_loc += Math.abs(ref.pairing_data.agr[2] - list[a].pairing_data.agr[2]);
-					console.log(list[a].id + "\t agr\t " + agr_loc);
-					if (agr_loc < agr_min) {
-						agr_min = agr_loc;
-						arg_out = list[a];
-					}
-
-					loc = 0;
-					loc += Math.abs(ref.pairing_data.std[0] - list[a].pairing_data.std[0]);
-					loc += Math.abs(ref.pairing_data.std[1] - list[a].pairing_data.std[1]);
-					loc += Math.abs(ref.pairing_data.std[2] - list[a].pairing_data.std[2]);
-					console.log(list[a].id + "\t std\t " + loc);
-					if (loc < min) {
-						min = loc;
-						out = list[a]
-					}
-
-				} else {
-					console.log(list[a].id + " no data")
-				}
+			for (var a = 0, max = out.pairs.length; a < max; ++a) {
+				out.pairs[a].emit("paired", msg)
 			}
-			console.log("\nsum agle somet of: " + min + "");
-			console.log("sum difference of: " + agr_min + "\n");
-			// console.log("agr choose " + arg_out + "")
-			return out;
+			out.this_socket.emit("paired", msg)
 		}
+		// message_pairs: function(message_name, data) {
+		// 	console.log("device: \t" + out.this_socket.id);
+		// 	if (!out.pairs.length) {
+		// 		console.log(out.this_socket.id + " found no pairs");
+		// 		return false;
+		// 	}
+		// 	out.this_socket.emit(message_name, data)
+		// 	for (var a = 0, max = out.pairs.length; a < max; ++a) {
+		// 		console.log(" --\t\t" + out.pairs[a].id);
+		// 		out.pairs[a].emit(message_name, data)
+		// 	}
+		// },
+		// message_winners: function(message_name, data) {
+		// 	console.log("winners: \t" + out.this_socket.id);
+		// 	console.log("\t\t" + out.paired.id);
+		// 	out.this_socket.emit(message_name, data);
+		// 	out.paired.emit(message_name, data);
+		// },
+		// message_losers: function(message_name, data) {
+		// 	for (var a = 0, max = out.pairs.length; a < max; ++a) {
+		// 		if (out.pairs[a].id != out.paired.id) {
+		// 			out.pairs[a].emit(message_name, data)
+		// 		}
+		// 	}
+		// },
+		// quarter_check: function() {
+		// 	console.log("\nquarter check")
+		// 	out.message_pairs("quarter check 5", out.pairs.length);
+		// },
 
+		// check: function() {
+		// 	console.log("\n\n\nfinal check " + out.this_socket);
+
+		// 	out.paired = out.find_closests_std(out.this_socket, out.pairs, 0.26, 0.1);
+
+		// 	if (!out.paired) {
+		// 		// out.this_socket.emit("final check 7", {
+		// 		// 	"status": "failed"
+		// 		// });
+
+		// 		out.message_pairs("final check 7", {
+		// 			"status": "failed"
+		// 		});
+		// 		// out.this_socket.pairing_data = null;
+		// 		return false;
+		// 	}
+
+		// 	out.paired.pairing_data.taken = true;
+
+		// 	console.log(out.this_socket.pairing_data.std)
+		// 	console.log(out.paired.pairing_data.std)
+		// 	if (out.pairs.length >= 1) {
+		// 		out.message_winners("final check 7", {
+		// 			"status": "success",
+		// 			"pairs": [out.this_socket.pairing_data.element, out.paired.pairing_data.element]
+		// 		});
+		// 		out.message_losers("final check 7", {
+		// 			"status": "failed"
+		// 		})
+		// 		// out.this_socket.pairing_data = null;
+		// 	} else {
+		// 		console.log("\n\n\n SOMETHING WENT VERY WRONG \n\n\n");
+		// 		out.message_pairs("final check 7", false);
+		// 	}
+
+		// },
+		// find_closests_std: function(ref, list, limit, agr) {
+		// 	var out = false,
+		// 		arg_out = false,
+		// 		min = limit || 99999999,
+		// 		loc = 0,
+		// 		agr_min = agr,
+		// 		agr_loc = 0;
+
+		// 	console.log("--\t" + ref.id)
+		// 	// console.log(ref.pairing_data.table)
+		// 	for (var a = 0, max = list.length; a < max; ++a) {
+
+		// 		// console.log(list[a].pairing_data.table)
+		// 		if (list[a].pairing_data && !list[a].pairing_data.taken && list[a].id != ref.id) {
+		// 			agr_loc = 0;
+		// 			agr_loc += Math.abs(ref.pairing_data.agr[0] - list[a].pairing_data.agr[0]);
+		// 			agr_loc += Math.abs(ref.pairing_data.agr[1] - list[a].pairing_data.agr[1]);
+		// 			agr_loc += Math.abs(ref.pairing_data.agr[2] - list[a].pairing_data.agr[2]);
+		// 			console.log(list[a].id + "\t agr\t " + agr_loc);
+		// 			if (agr_loc < agr_min) {
+		// 				agr_min = agr_loc;
+		// 				arg_out = list[a];
+		// 			}
+
+		// 			loc = 0;
+		// 			loc += Math.abs(ref.pairing_data.std[0] - list[a].pairing_data.std[0]);
+		// 			loc += Math.abs(ref.pairing_data.std[1] - list[a].pairing_data.std[1]);
+		// 			loc += Math.abs(ref.pairing_data.std[2] - list[a].pairing_data.std[2]);
+		// 			console.log(list[a].id + "\t std\t " + loc);
+		// 			if (loc < min) {
+		// 				min = loc;
+		// 				out = list[a]
+		// 			}
+
+		// 		} else {
+		// 			console.log(list[a].id + " no data")
+		// 		}
+		// 	}
+		// 	console.log("\nsum agle somet of: " + min + "");
+		// 	console.log("sum difference of: " + agr_min + "\n");
+		// 	// console.log("agr choose " + arg_out + "")
+		// 	return out;
+		// }
 	};
 
 	return out;
@@ -150,7 +163,7 @@ var possible_pairs_root = function(main_socket) {
 
 var time = {
 	point: [], // data is store in there [[time,socket],[time,socket],[time,socket]]
-	time_size: 2000,
+	time_size: 1500,
 	checker_interval: 100,
 	init: function() {
 		game.io.sockets.on('connection', time.connect);
@@ -162,55 +175,41 @@ var time = {
 	// when we get data of a certain type we test for pairs
 	// we test against the time.point array
 	connect: function(socket) {
-		socket.on('data', function(data) {
-			if (data.type == "tap message 1") {
-				socket.possible_pairs = null;
-				// we ask the client to wait till they are out of the time pairing zome
-				socket.emit("wait 2", time.time_size * 2);
-				if (time.point.length) {
-					socket.possible_pairs = possible_pairs_root(socket);
-					for (var a = 0, max = time.point.length; a < max; ++a) {
-						if (time.point[a][1]["id"] != socket["id"]) {
-							// console.log("\n\nFOUND AN PAIR!!!\n\n" + socket.id + " and " + time.point[a][1].id + "\n\n")
-							socket.possible_pairs.pairs.push(time.point[a][1]);
-						} else {
-							time.point.splice(a, 1);
-							a -= 1;
-							max -= 1;
+
+
+		socket.on('join queue', function(data) {
+			console.log("\t\t" + socket.id + " joined queue")
+			socket.emit("joined queue", time.time_size);
+			socket.possible_pairs = possible_pairs_root(socket);
+
+			if (time.point.length) {
+				for (var a = 0, max = time.point.length; a < max; ++a) {
+					if (time.point[a][1]["id"] != socket["id"]) {
+						socket.possible_pairs.pairs.push(time.point[a][1]);
+						if (!time.point[a][1].messaged_to_continue) {
+							time.point[a][1].emit("continue to record", socket.id);
+							time.point[a][1].messaged_to_continue = true;
 						}
+						if (!socket.messaged_to_continue) {
+							socket.emit("continue to record", "continue");
+						}
+					} else {
+						time.point.splice(a, 1);
+						a -= 1;
+						max -= 1;
 					}
-					socket.possible_pairs.message_pairs("time paired 3", "worked!");
-				} else if (use_fake_sockets) {
-					socket.possible_pairs = possible_pairs_root(socket);
-					socket.possible_pairs.message_pairs("time paired 3", "worked!");
-				};
-				time.point.push([new Date().getTime(), socket]);
-			};
-		});
-		socket.on('paired data 4', function(data) {
-			// console.log("\n\t\tquart data")
-			// console.log(data["raw"]);
-			socket.pairing_data = data;
-			if (socket.possible_pairs) {
-				socket.possible_pairs.quarter_check();
-			};
-		});
-		socket.on('paired data 6', function(data) {
-			if (socket.pairing_data) {
-				socket.emit("stop", {});
-			};
-			socket.pairing_data = data;
-			if (!socket.possible_pairs || !socket.possible_pairs.pairs) {
-				console.log("final check no pair " + socket.id);
-				return false;
+				}
+				// socket.possible_pairs.message_pairs("time paired 3", "worked!");
 			}
-			if (socket.possible_pairs && socket.possible_pairs.pairs.length) {
-				console.log("\n\nfinalcheck");
-				// socket.possible_pairs.print_tables();
-				// console.log(socket.pairing_data.table);
-				// console.log(socket.possible_pairs.pairs[0].pairing_data.table);
-				socket.possible_pairs.check();
-			};
+
+
+			time.point.push([new Date().getTime(), socket]);
+
+		});
+
+		socket.on('recording finished', function(data) {
+			console.log("\t" + socket.id + " sent final data");
+			socket.possible_pairs.check();
 		});
 	},
 
@@ -224,6 +223,7 @@ var time = {
 			for (var i = time.point.length - 1; i >= 0; i--) {
 				// console.log("\t\t" + time.point[i][0])
 				if (time.point[i][0] + time.time_size < t) {
+					console.log("\t\t" + time.point[i][1].id + " left queue")
 					time.point.splice(i, 1);
 					i -= 1;
 				}
